403741388a1f67961274acc814e33e6ed18c076e
luisdavidog <35609458+luisdavidog@users.noreply.github.com>
2023-01-07 20:56:37 -0600

    cambios en login

--- index.php
+++ index.php
@@ -8,46 +8,14 @@
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-<?php
-    require('db.php');
-    session_start();
-    // When form submitted, check and create user session.
-    $username = stripslashes($_REQUEST['usuario']);    // removes backslashes
-    $username = mysqli_real_escape_string($con, $username);
-    $password = md5($_REQUEST['contra']);
-    $password = mysqli_real_escape_string($con, $password);
 
-    if (isset($_POST['usuario'])) {
-        // Check user is exist in the database
-        $query    = "SELECT * FROM `usuario` WHERE usuario='$username'
-                     AND contra='$password'";
-        $result = mysqli_query($con, $query);
-        $rows = mysqli_num_rows($result);
-        if ($rows == 1) {
-            $_SESSION['usuario'] = $username;
-            echo '<script type ="text/JavaScript">';  
-          echo 'alert("Login Exitoso")';  
-          echo '</script>';
-          echo '<div class="fs-1">Reedirigiendo...</div>';
-            // Redirect to user dashboard page
-            header("refresh:2;url=trabajadores/Principal");
-        } else {
-            echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
-                  <p class='link'>Click here to <a href='index.php'>Login</a> again.</p>
-                  </div>";
-        }
-    } else {
-?>
-    <form class="form-floating mb-2" method="post" name="login">
+    <form action="login.php" class="form-floating mb-2" method="post" name="login">
         <h1 id="login-title">Login</h1>
         <center><img src="assets/Password_Flatline.svg" alt="" srcset=""></center>
         <input type="text" id="login-input" name="usuario" class="form-control" placeholder="Usuario" autofocus="true"/>
         <input type="password" id="login-input" name="contra" class="form-control" placeholder="Contraseña"/>
         <input type="submit" value="inicio" name="submit" id="login-button" class="btn btn-primary"/>
   </form>
-<?php
-    }
-?>
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,31 +1,28 @@
 <?php
   require('db.php');
   session_start();
-   
-  if (isset($_POST['usuario'])) {
-    $username = stripslashes($_REQUEST['usuario']);    // removes backslashes
-    $username = mysqli_real_escape_string($con, $username);
-    $password = stripslashes($_REQUEST['contra']);
-    $password = mysqli_real_escape_string($con, $password);
-    // Check user is exist in the database
-    $query    = "SELECT * FROM `usuario` WHERE usuario='$username'
-                 AND contra='" . md5($password) . "'";
-    $result = mysqli_query($con, $query);
-    $rows = mysqli_num_rows($result);
-   
-  // Verificando si el usuario existe en la base de datos.
-  if($rows == 1){
-    // Guardo en la sesión el email del usuario.
-    $_SESSION['usuario'] = $username;
-     
-    // Redirecciono al usuario a la página principal del sitio.
-    echo '<script type ="text/JavaScript">';  
-          echo 'alert("Login Exitoso")';  
-          echo '</script>';
-          echo '<div class="fs-1">Reedirigiendo...</div>';
-    header("refresh:3;url=trabajadores/Principal");
-  }else{
-    echo 'El email o password es incorrecto, <a href="index.html">vuelva a intenarlo</a>.<br/>';
-  }
-}
+  // When form submitted, check and create user session.
+  $username = stripslashes($_REQUEST['usuario']);    // removes backslashes
+  $username = mysqli_real_escape_string($con, $username);
+  $password = md5($_REQUEST['contra']);
+  $password = mysqli_real_escape_string($con, $password);
+
+      $query    = "SELECT * FROM `usuario` WHERE usuario='$username'
+                   AND contra='$password'";
+      $result = mysqli_query($con, $query);
+      $rows = mysqli_num_rows($result);
+      if ($rows == 1) {
+          $_SESSION['usuario'] = $username;
+          echo '<script type ="text/JavaScript">';  
+        echo 'alert("Login Exitoso")';  
+        echo '</script>';
+        echo '<div class="fs-1">Reedirigiendo...</div>';
+          // Redirect to user dashboard page
+          header("refresh:2;url=trabajadores/Principal");
+      } else {
+          echo "<div class='form'>
+                <h3>Incorrect Username/password.</h3><br/>
+                <p class='link'>Click here to <a href='index.php'>Login</a> again.</p>
+                </div>";
+      }
 ?>
